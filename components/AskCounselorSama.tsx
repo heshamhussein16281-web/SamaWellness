26c6a8449d492f6760b367641585146f451b3bea
Sama Eissa <samaeissa@Samas-MacBook-Air-New.local>
2026-06-12 11:52:10 +0300

    Update Ask Sama section to use portrait-only photo of Sama
    Changed image from full reference design to just Sama's portrait
    - New image: BA1107F8-532E-4255-9A4D-BF42D229EAED.png
    - Shows only the lady in black suit, no background text
    - Cleaner, more focused visual on Ask Sama page

--- components/AskCounselorSama.tsx
+++ components/AskCounselorSama.tsx
@@ -125,7 +125,7 @@ export default function AskCounselorSama() {
         <div className="ask-sama-photo-col">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src="/PHOTO-2026-06-11-16-13-47.jpg"
+            src="/BA1107F8-532E-4255-9A4D-BF42D229EAED.png"
             alt="Sama Eissa - Founder & Counselor"
             className="ask-sama-photo"
           />
